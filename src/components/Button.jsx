22f7603bd9ee6364cdf5a5e4b9ed16f65fc23c16
Arbaz001 <arbaza3212btechcse2025@kccitm.edu.in>
2024-11-05 21:14:52 +0530

    1.00.0

---
+++ src/components/Button.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+
+const Button = () => {
+    return <button>Add Todo</button>;
+};
+export default Button;
---
+++ src/components/todo_item.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoItem = () => {
+    return (
+        <li>
+            <input type="checkbox" name="" id="" />
+            <p>Eat</p>
+            <p>...</p>
+        </li>
+    );
+};
+export default TodoItem;
